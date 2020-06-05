06cea129a6064badd2e8e335cc134752b0e07782
strikingloo <luciano.strika@mercadolibre.com>
2020-06-05 11:58:54 -0300

    add functionality for complete sentences, and argparsing.

--- wotd_jp.py
+++ wotd_jp.py
@@ -1,7 +1,12 @@
 import pandas as pd
+import argparse
 
-df = pd.read_csv('~/Documents/fun/jp_wotd/jlpt3_vocab.csv')
-df.dropna(inplace=True)
+words_df = pd.read_csv('~/Documents/fun/jp_wotd/jlpt3_vocab.csv')
+words_df.dropna(inplace=True)
+
+sentences_df = pd.read_csv('sentences_clean.csv')
+
+
 def word(df):
 	return dict(df.sample(1).iloc[0])
 
@@ -9,4 +14,18 @@ def show_word(df):
     a_word = word(df)
     print(a_word['word']+':\n\t'+a_word['hiragana']+'\n\t'+a_word['meaning'])
 
-show_word(df)
+def show_sentence(df):
+	print(df.sample(1).iloc[0]['sentence'])
+
+parser = argparse.ArgumentParser(description='Improve your Japanese vocabulary.')
+
+parser.add_argument('-w', '--word', help='Get a random Japanese word', action='store_const', const=True)
+parser.add_argument('-s', '--sentence', help='Get a random Japanese sentence', action='store_const', const=True)
+
+
+ns = parser.parse_args()
+if ns.sentence:
+	print('Sentence of the day: ')
+	show_sentence(sentences_df)
+if ns.word:	
+	show_word(words_df)
